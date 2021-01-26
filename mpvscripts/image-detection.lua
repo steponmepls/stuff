a9d3d32b6f7900313f89e168ae4fb2469044b7b4
steponmepls <leewdch@gmail.com>
2021-01-26 17:43:26 +0100

    Added reset profile when moving from image to video with audio

--- mpvscripts/image-detection.lua
+++ mpvscripts/image-detection.lua
@@ -4,16 +4,16 @@
 local was_image
 
 mp.register_event('file-loaded', function()
-    local container_fps = mp.get_property_number('container-fps')
+	local container_fps = mp.get_property_number('container-fps')
 	local file_format = mp.get_property('video-format')
 	-- check if still image but
 	if (container_fps == 1 or container_fps == nil)
 	-- also consider animated webm/gif
 	or (file_format == 'vp8' or file_format == 'vp9' or file_format == 'gif')
 	--  without audio
-        and not mp.get_property('audio-codec') then
-        -- mp.command('show-text "[${playlist-pos-1}/${playlist-count}] ${filename} ${width}x${height} ${!gamma==0:☀}" 3000')
-        -- Or set osd-msg1 to show text permanently.
+	and not mp.get_property('audio-codec') then
+	-- mp.command('show-text "[${playlist-pos-1}/${playlist-count}] ${filename} ${width}x${height} ${!gamma==0:☀}" 3000')
+	-- Or set osd-msg1 to show text permanently.
 
         if not was_image then
             -- mp.set_property('video-unscaled', 'yes')
@@ -25,6 +25,7 @@ mp.register_event('file-loaded', function()
         --mp.set_property('video-unscaled', 'no')
         mp.set_property('video-zoom', 0)
         mp.set_property('panscan', 0)
+		mp.command('apply-profile reset')
         mp.command('disable-section image')
         was_image = false
     end
