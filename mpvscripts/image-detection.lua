3a0f25eb9c62b950ac6d5e81d4615f4d6c730e48
steponmepls <63500907+steponmepls@users.noreply.github.com>
2021-01-26 17:37:06 +0100

    Adjusted formatting

--- mpvscripts/image-detection.lua
+++ mpvscripts/image-detection.lua
@@ -8,16 +8,16 @@ mp.register_event('file-loaded', function()
 	local file_format = mp.get_property('video-format')
 	-- check if still image but
 	if (container_fps == 1 or container_fps == nil)
-		-- also consider animated webm/gif
-		or (file_format == 'vp8' or file_format == 'vp9' or file_format == 'gif')
-		--  without audio
+	-- also consider animated webm/gif
+	or (file_format == 'vp8' or file_format == 'vp9' or file_format == 'gif')
+	--  without audio
         and not mp.get_property('audio-codec') then
         -- mp.command('show-text "[${playlist-pos-1}/${playlist-count}] ${filename} ${width}x${height} ${!gamma==0:☀}" 3000')
         -- Or set osd-msg1 to show text permanently.
 
         if not was_image then
             -- mp.set_property('video-unscaled', 'yes')
-			mp.command('apply-profile image')
+            mp.command('apply-profile image')
             mp.command('enable-section image')
             was_image = true
         end
