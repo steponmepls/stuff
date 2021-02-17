aa4ca2382956881799b90919a2d5337bda44c297
steponmepls <leewdch@gmail.com>
2021-02-17 16:31:02 +0100

    Added run-at condition so that script doesn't miss the init event from 4chan X API anymore

--- userscripts/qr-random.user.js
+++ userscripts/qr-random.user.js
@@ -3,41 +3,32 @@
 // @include https://boards.4chan.org/*
 // @include https://boards.4channel.org/*
 // @grant none
+// @run-at document-start
 // ==/UserScript==
 
-document.addEventListener("4chanXInitFinished", function() {
-    //console.log("Init")
-    if (document.querySelector("#qr")) {
-        initRandomizer()
-        // fallback for non-persistent QR configs
-        document.addEventListener("QRDialogCreation", function() {
-            initRandomizer()
-        })
-    }
-});
-
-// dirtier fallback for those times 4chanX init doesn't fire somehow
-setTimeout(function() {
-    if (document.querySelector("#qr")) {
-      initRandomizer()
-    }
-  }, 3000);
-
-function initRandomizer() {
-    var container = document.querySelector("#qr-filename-container")
-    var filename = container.querySelector("#qr-filename")
-    if (!container.querySelector("#randomizer")) {
-        var randomizer = document.createElement("a")
-        randomizer.id = "randomizer"
-        randomizer.setAttribute("title", "Randomize filename")
-        randomizer.setAttribute("aria-hidden", "true")
-        randomizer.setAttribute("class", "fa fa-hashtag")
-        randomizer.style.opacity = ".6"
-        randomizer.onclick = function() {
-            $.DAY = 24 * ($.HOUR = 60 * ($.MINUTE = 60 * ($.SECOND = 1000)))
-            let newFilename = (Date.now() - Math.floor(Math.random() * 365 * $.DAY))
-            filename.value = newFilename
-        }
-        container.appendChild(randomizer)
-    }
-};
+(function () {
+    document.addEventListener("4chanXInitFinished", initTweaks);
+    document.addEventListener("QRDialogCreation", initTweaks);
+    function initTweaks() {
+        if (document.querySelector("#qr")) {
+            var qr = document.querySelector("#qr");
+            // Randomizer in QR
+            let container = qr.querySelector("#qr-filename-container");
+            let filename = container.querySelector("#qr-filename");
+            if (!container.querySelector("#randomizer")) {
+                var randomizer = document.createElement("a");
+                randomizer.id = "randomizer";
+                randomizer.setAttribute("title", "Randomize filename");
+                randomizer.setAttribute("aria-hidden", "true");
+                randomizer.setAttribute("class", "fa fa-hashtag");
+                randomizer.style.opacity = ".6";
+                randomizer.onclick = function() {
+                    $.DAY = 24 * ($.HOUR = 60 * ($.MINUTE = 60 * ($.SECOND = 1000)))
+                    let newFilename = (Date.now() - Math.floor(Math.random() * 365 * $.DAY))
+                    filename.value = newFilename
+                };
+                container.appendChild(randomizer);
+            };
+        };
+    };
+})();
